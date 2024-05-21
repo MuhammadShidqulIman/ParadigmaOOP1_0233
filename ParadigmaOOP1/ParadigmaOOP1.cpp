b80dd5ead5a2bd609c81d1c10f22cfd38f967e0f
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-05-21 14:52:20 +0700

    Membuat class mahasiswa private

--- ParadigmaOOP1/ParadigmaOOP1.cpp
+++ ParadigmaOOP1/ParadigmaOOP1.cpp
@@ -2,6 +2,13 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
